eee8e9988e27c511bbb63f3dd3be3fdb7218d099
davidv <davidvazquezgit@gmail.com>
2022-10-21 07:42:33 -0700

    updated deck Model

--- models/deck.js
+++ models/deck.js
@@ -2,19 +2,10 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
-const cardSchema = new Schema({
-  name: String,
-  imgUrl: String,
-  cmc: Number,
-  colorIdentity: String
-},{
-  timestamps: true,
-})
-
 const deckSchema = new Schema({
   title: String,
-  cards: [cardSchema],
-  author: {type: Schema.Types.ObjectId, ref: 'Comment'}
+  cards: {type: Schema.Types.ObjectId, ref: 'Cards'},
+  comments: {type: Schema.Types.ObjectId, ref: 'Comment'}
 },{
   timestamps: true,
 })
