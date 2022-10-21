b740fedc1811209833c2e6897f67177c7630524c
EricSoklowski <ericsoko2002@gmail.com>
2022-10-21 14:24:58 -0400

    stub up deck controller

---
+++ controllers/deck.js
@@ -0,0 +1,35 @@
+import { Profile } from "../models/profile";
+import { Deck } from '../models/deck.js'
+
+const create = async (req, res) => {
+
+}
+
+const index = async (req, res) => {
+
+}
+
+const show = async (req, res) => {
+
+}
+
+const update = async (req, res) => {
+  
+}
+
+const deleteDeck = async (req, res) => {
+  
+}
+
+const createComment = async (req, res) => {
+  
+}
+
+export {
+  create,
+  index,
+  show,
+  update,
+  deleteDeck as delete,
+  createComment,
+}
