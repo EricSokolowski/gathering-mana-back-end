f4c564327a87b9a4fcc646c090e0076c12ce68cb
davidv <davidvazquezgit@gmail.com>
2022-10-21 11:08:59 -0700

    create decks routes

---
+++ routes/decks.js
@@ -0,0 +1,24 @@
+import { Router } from 'express'
+import * as decksCtrl from '../controllers/blogs.js'
+import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
+
+const router = Router()
+
+// ========== Public Routes ===========
+router.get('/', decksCtrl.index)
+
+// ========= Protected Routes ========= 
+router.use(decodeUserFromToken)
+
+
+router.get('/:id', checkAuth, decksCtrl.show)
+
+router.post('/', checkAuth, decksCtrl.create)
+router.post('/:id/comments', checkAuth, decksCtrl.createComment)
+
+router.put('/:id', checkAuth, decksCtrl.update)
+
+router.delete('/:id', checkAuth, decksCtrl.delete)
+
+
+export { router }
